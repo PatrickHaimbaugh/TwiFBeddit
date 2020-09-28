ad6c4c2fa5ab77bdd0cdd77fed64c4a8ea61a5cd
Patrick Haimbaugh <patrick.haimbaugh@blindrobot.com>
2020-09-28 16:28:32 -0400

    Added Redux and basic file structure

--- twifbeddit-app/src/App.js
+++ twifbeddit-app/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import { useSelector } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+	const currentPage = useSelector((state) => state.navigation.currentPage);
 
+	return <div>{currentPage === "LandingPage" && <LandingPage />}</div>;
+};
 export default App;
---
+++ twifbeddit-app/src/containers/NavigationContainer/constants.js
@@ -0,0 +1,1 @@
+export const CHANGE_CURRENT_PAGE = "NavigationContainer/CHANGE_CURRENT_PAGE";
