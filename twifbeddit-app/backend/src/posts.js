2f1b2df4cbeca526d65ee318100994101fb10eb4
jtruo <jazure622@gmail.com>
2020-11-02 17:07:49 -0500

    Added timeline and removed limit of posts returned

--- twifbeddit-app/backend/src/posts.js
+++ twifbeddit-app/backend/src/posts.js
@@ -50,7 +50,7 @@ exports.GET = async (_, event) => {
     if (event.queryStringParameters == null) {
         const username = await get_user_from_header(event.headers);
         const user = await User.findOne({username: username});
-        var posts = await Post.find().where('topic').in(user.followed_topics).limit(8).sort({ createdAt: -1}).exec();
+        var posts = await Post.find().where('author').in(user.following).where('topic').in(user.followed_topics).sort({ createdAt: -1}).exec();
         posts = JSON.parse(JSON.stringify(posts));
         for (var post of posts) {
             createExternalPost(post);
@@ -66,7 +66,7 @@ exports.GET = async (_, event) => {
     } 
     if (event.queryStringParameters.topic != undefined) {
         const topic = event.queryStringParameters.topic;
-        var posts = await Post.find().where('topic').in(topic).limit(8).sort({ createdAt: -1}).exec();
+        var posts = await Post.find().where('topic').in(topic).sort({ createdAt: -1}).exec();
         posts = JSON.parse(JSON.stringify(posts));
         for (var post of posts) {
             createExternalPost(post);
